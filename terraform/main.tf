6a6ba7cc462fdc63a3b23ff9fc30495df1791023
ruthsrur <ruthsrur@gmail.com>
2024-08-24 13:00:49 +0300

    changing jenkinsfile

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "instance_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["127.0.0.1/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
