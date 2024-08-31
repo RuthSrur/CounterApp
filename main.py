1772876de0940a1f231df972ffa976d13fe441ac
ruthsrur <ruthsrur@gmail.com>
2024-08-31 11:17:14 +0300

    add button

--- main.py
+++ main.py
@@ -6,17 +6,23 @@ counter = 0
 
 @app.route("/", methods=["GET"])
 def main():
-    return render_template("home.html"), 200
+    return render_template("home.html", counter=counter), 200
 
 
-@app.route("/increment", methods=["POST", "GET"])
-def postcounter():
+@app.route("/add", methods=["POST"])
+def add_counter():
     global counter
-    if request.method == "POST":
-        counter += 1
-    print(counter)
+    counter += 1
+    return render_template("home.html", counter=counter)
+
+
+@app.route("/subtract", methods=["POST"])
+def subtract_counter():
+    global counter
+    counter -= 1
     return render_template("home.html", counter=counter)
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8081)
+
